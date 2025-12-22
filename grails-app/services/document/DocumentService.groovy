3d600504561948412113ac8300ea79775f9b198f
Gabriel Ferreira do Prado <thegaprado@gmail.com>
2025-12-22 18:34:47 -0300

    Adicionando Service para extrair texto dos documentos

---
+++ grails-app/services/document/DocumentService.groovy
@@ -0,0 +1,38 @@
+package document
+
+import grails.gorm.transactions.Transactional
+
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.text.PDFTextStripper
+import org.springframework.web.multipart.MultipartFile
+
+@Transactional
+class DocumentService {
+
+    public String extractNormalizedTextFromPDF(MultipartFile pdfFile) {
+        if (!pdfFile || pdfFile.empty) {
+            return ""
+        }
+
+        PDDocument document = PDDocument.load(pdfFile.inputStream)
+
+        String text = extractText(document)
+
+        return normalize(text)
+    }
+
+    private String extractText(PDDocument document) {
+        PDFTextStripper textStripper = new PDFTextStripper()
+        textStripper.setSortByPosition(true)
+
+        return textStripper.getText(document)
+    }
+
+    private String normalize(String text) {
+        return text
+                .toLowerCase()
+                .replaceAll("[^a-z0-9\\s]", " ")
+                .replaceAll("\\s+", " ")
+                .trim()
+    }
+}
