8d1e75d45c073c4845835f44169701be7a004d87
Gabriel Ferreira do Prado <thegaprado@gmail.com>
2025-12-23 18:23:33 -0300

    Adicionando StopwordUtils

---
+++ src/doctwin/utils/StopwordUtils.groovy
@@ -0,0 +1,20 @@
+package doctwin.utils
+
+class StopwordUtils {
+
+    private static final Set<String> PT = [
+            "a", "o", "e", "de", "do", "da", "que", "para", "com", "no", "na",
+            "por", "em", "um", "uma", "os", "as", "se", "ou", "ao", "aos",
+            "como", "mais", "mas", "foi", "ser", "tem", "há", "já", "não"
+    ] as Set
+
+    private static final Set<String> EN = [
+            "the", "and", "of", "to", "for", "in", "on", "with", "is", "are",
+            "was", "were", "be", "by", "that", "this", "it", "as", "from",
+            "or", "an", "at", "which", "but", "not"
+    ] as Set
+
+    static Boolean isStopword(String token) {
+        return PT.contains(token) || EN.contains(token)
+    }
+}
